a75b11874dc9e340df2d375b0be0d4ad30ce3e07
Segopotso <leketi.s@gmail.com>
2019-08-13 21:35:44 +0200

    Kmeans first commit, already added method for summing

---
+++ KMeans.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int sum(vector<int> input);
+vector<double> computeMean(vector<int> xInput, vector<int> yInput, vector<double> kMean);
+
+int main(){
+
+
+}
+
